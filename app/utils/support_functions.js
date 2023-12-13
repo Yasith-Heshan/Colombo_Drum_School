8703189f923a5a1e6b61d15003a2b29b07a3e962
Yasith <yheshan1@gmail.com>
2023-12-13 16:55:05 +0530

    fix navbar error

--- app/utils/support_functions.js
+++ app/utils/support_functions.js
@@ -3,7 +3,5 @@ export const shortenName = (fullName)=>{
     const firstName = nameArray[0];
     const lastName = nameArray.length > 1 ? nameArray[nameArray.length - 1] : '';
 
-    const shortenedName = `${firstName} ${lastName.charAt(0)}.`;
-
-    return shortenedName;
+    return `${firstName} ${lastName.charAt(0)}.`;
 }
